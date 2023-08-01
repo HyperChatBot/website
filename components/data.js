3c8a49ec19aad93d3e37103593c0df6bbf01425f
YanceyOfficial <yanceyofficial@gmail.com>
2023-08-01 15:35:27 +0800

    feat: release the first version of landing page

--- components/data.js
+++ components/data.js
@@ -1,40 +1,40 @@
 const benefitOne = {
   title: "Chat Completion",
   desc: "Chat models take a series of messages as input, and return a model-generated message as output.",
-  lightImage: "https://edge.yancey.app/beg/qk79imya-1685073643946.png",
-  darkImage: "https://edge.yancey.app/beg/usgq0hjm-1685018446761.png",
+  lightImage: "https://edge.yancey.app/beg/gf3ohoi2-1690808811131.png",
+  darkImage: "https://edge.yancey.app/beg/frflci8f-1690808815033.png",
   bullets: [],
 };
 
 const benefitTwo = {
   title: "Text Completion",
   desc: "The completions endpoint can be used for a wide variety of tasks. You input some text as a prompt, and the model will generate a text completion that attempts to match whatever context or pattern you gave it.",
-  lightImage: "https://edge.yancey.app/beg/2fpf1pv6-1685074617343.png",
-  darkImage: "https://edge.yancey.app/beg/z5rjy1r5-1685076020111.png",
+  lightImage: "https://edge.yancey.app/beg/1rkhknyn-1690808805630.png",
+  darkImage: "https://edge.yancey.app/beg/9syfyonp-1690808800955.png",
   bullets: [],
 };
 
 const benefitThree = {
   title: "Audio Transcription",
   desc: "The transcriptions API takes as input the audio file you want to transcribe and the desired output file format for the transcription of the audio.",
-  lightImage: "https://edge.yancey.app/beg/q8tjqu1k-1685073591563.png",
-  darkImage: "https://edge.yancey.app/beg/k7mo62kq-1685076012796.png",
+  lightImage: "https://edge.yancey.app/beg/3wltjhro-1690808789504.png",
+  darkImage: "https://edge.yancey.app/beg/ajh4qoaa-1690808795519.png",
   bullets: [],
 };
 
 const benefitFour = {
   title: "Audio Translation",
   desc: "The translations API takes as input the audio file in any of the supported languages and transcribes, if necessary, the audio into english.",
-  lightImage: "https://edge.yancey.app/beg/c7ppn5s6-1685074011821.png",
-  darkImage: "https://edge.yancey.app/beg/zdmmm1kq-1685076004067.png",
+  lightImage: "https://edge.yancey.app/beg/zxbsckf9-1690808783373.png",
+  darkImage: "https://edge.yancey.app/beg/oruwxioq-1690808776343.png",
   bullets: [],
 };
 
 const benefitFive = {
   title: "Text To Image",
   desc: "The image generations endpoint allows you to create an original image given a text prompt.",
-  lightImage: "https://edge.yancey.app/beg/u1odt2ot-1685074261182.png",
-  darkImage: "https://edge.yancey.app/beg/jlapw99s-1685075993402.png",
+  lightImage: "https://edge.yancey.app/beg/9oqb91zz-1690808763929.png",
+  darkImage: "https://edge.yancey.app/beg/uo6x2952-1690808769662.png",
   bullets: [],
 };
 
